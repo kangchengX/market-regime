Correct the calculation of the normalized entropy

stats.py
@@ -1,25 +1,6 @@
 import numpy as np
 import pandas as pd
-from typing import Dict, Union
-
-# def calculate_regime_durations(regimes: np.ndarray) -> Dict[int, np.ndarray]:
-#     '''Calculate regime durations.
-
-#     Args:
-#         regimes: regime labels for each time stamp
-
-#     Returns:
-#         regimes_durations: dict with reimges as keys and arrays of durations as values
-#     '''
-
-#     change_indices = np.where(np.diff(regimes) != 0)[0] + 1
-#     durations = np.diff(np.concatenate(([0],change_indices,[len(regimes)])))
-#     # the coresponding regime for that duration
-#     durations_regimes = np.concatenate((regimes[0:1],regimes[change_indices]))
-
-#     regimes_durations = {regime:durations[durations_regimes==regime] for regime in np.unique(regimes)}
-
-#     return regimes_durations
+from typing import Union
 
 
 def calculate_regime_durations(regimes_df: pd.DataFrame) -> pd.DataFrame:
@@ -27,29 +8,30 @@ def calculate_regime_durations(regimes_df: pd.DataFrame) -> pd.DataFrame:
     Calculate regime durations.
     
     Args:
-        regimes_df (DataFrame): DateFrame that includes columns ('DATE', 'regime').
+        regimes_df (DataFrame): DateFrame that includes columns (`'DATE'`, `'regime'`). Other columns will be ignored.
 
-    Return:
-        regime_durations (DataFrame): DateFrame only with columns ('regime', 'period', 'duration').
+    Returns:
+        regime_durations (DataFrame): DateFrame only with columns (`'regime'`, `'period'`, `'duration'`).
     """
-    df_inter = regimes_df[['DATE', 'regime']]
-    df_inter['regime_if_new'] = df_inter['regime'] != df_inter['regime'].shift(1)
-    df_inter['period'] = df_inter['regime_if_new'].cumsum()
+    regimes_df = regimes_df[['DATE', 'regime']]
+    regimes_df['regime_if_new'] = regimes_df['regime'] != regimes_df['regime'].shift(1)
+    regimes_df['period'] = regimes_df['regime_if_new'].cumsum()
 
-    regime_durations = df_inter.groupby(by=['regime', 'period']).size().reset_index(name='duration')
+    regime_durations = regimes_df.groupby(by=['regime', 'period']).size().reset_index(name='duration')
 
     return regime_durations
 
 
 def calculate_regime_durations_statistics(regime_durations: pd.DataFrame) -> pd.DataFrame:
     """
-    Calculate mean, std and quantiles for regime durations for each regime and the whole period
+    Calculate mean, std, 25, 50, 70 quantiles for regime durations and the total days and the corresponding percentage for each regime and all regime periods.
     
     Args:
-        regimes_durations (DataFrame): dataframe that includes columns ('regime', 'duration'). Note, the duration here is the duration of the period when the regime keeps the same. Not the total duration.
+        regimes_durations (DataFrame): dataframe that includes columns (`'regime'`, `'duration'`). Other columns will be ignored.
+            Note, the 'duration'` is the duration of the period when the regime keeps the same. Not the total duration of a regime.
 
     Returns:
-        statistics (DataFrame): dataframe with columns ('regime', 'mean','std','count','25 quantile', 'median', '75 quantile', 'percent')
+        statistics (DataFrame): dataframe with columns (`'regime'`, `'mean'`, `'std'`, `'count'`, `'25 quantile'`, `'median'`, `'75 quantile'`, `'percent'`).
     """
 
     total_dates = regime_durations['duration'].sum()
@@ -76,6 +58,7 @@ def calculate_regime_durations_statistics(regime_durations: pd.DataFrame) -> pd.
 
     # to DateFrame
     statistics = pd.DataFrame.from_dict(statistics, orient='index', columns=['mean','std','count', '25 quantile','median','75 quantile'])
+    
     # add 'percent'
     statistics['percent'] = statistics['count'] / total_dates 
     statistics = statistics.reset_index(names='regime')
@@ -86,21 +69,26 @@ def calculate_regime_durations_statistics(regime_durations: pd.DataFrame) -> pd.
 def calculate_return_metrics(
         df: pd.DataFrame, 
         freq_alias: str | None = None, 
-        freq_length: int | None = 1):
-    '''Calculate average return, std, annualized return, annualized std, sharpe ratio
-    
-    Args:
-        df: the data with and only with columns (DATE, .......) (the other columns are returns)
-        freq_alias: alias for the length of the period, can be 'W', '2W', 'ME', default is None for 1 day
-            full list see https://pandas.pydata.org/pandas-docs/stable/user_guide/timeseries.html#offset-aliases
-        freq_length: length of the period (in days)
+        freq_length: int | None = 1
+):
+    """
+    Calculate 5 metrics for returns - average return, std, annualized return, annualized std, sharpe ratio. Note, the return period used for
+    the average return and std are givin by `freq_alias` and `freq_length` (and alias and length should be constant). For example,
+    alias `'W'` and length 5 for average weekly return.
 
-    Return:
-        metrics: dataframe with indices as indices and metrics as columns
-    '''
+    Args:
+        df (DataFrame): the data only with columns (`'DATE'`, ...) (the other columns are returns).
+        freq_alias (str | None): alias for the length of the return period for average return and std. This can be `'W'`, `'2W'`, `'ME'`. 
+            Default is `None` for 1 day. Full list see https://pandas.pydata.org/pandas-docs/stable/user_guide/timeseries.html#offset-aliases.
+        freq_length (int): length of the return period (in days). Default to `1`.
+
+    Returns:
+        metrics (DataFrame): dataframe with indices as indexes and metrics as columns.
+    """
 
     # group by frequency
     if freq_alias is not None:
+        # not to use sum() here because there may be some missing data during the workdaying during the return period
         df_returns = df.groupby(pd.Grouper(key='DATE',freq=freq_alias),sort=False).mean()*freq_length
     else:
         df_returns = df.drop(columns='DATE')
@@ -128,19 +116,20 @@ def calculate_return_metrics_within_regime(
         freq_alias: str | None = None, 
         freq_length: int | None = 1
 ) ->pd.DataFrame:
-    '''Calculate average return, std, annualized return, annualized std, sharpe ratio within each regime
+    """
+    Calculate 5 metrics for returns - average return, std, annualized return, annualized std, sharpe ratio within each regime. 
+    Note, the return period used for the average return and std are givin by `freq_alias` and `freq_length` (and alias and length should be constant). 
+    For example, alias `'W'` and length 5 for average weekly return.
     
     Args:
-        df: the data with and only with columns (DATE, 'regime', ...) (the other columns are returns)
-        regimes_df: DateFrame of the regimes with columns('DATE', 'regime')
-        freq_alias: alias for the length of the period, can be 'W', '2W', 'ME'. default is None for 1 day
-            full list see https://pandas.pydata.org/pandas-docs/stable/user_guide/timeseries.html#offset-aliases
-        freq_length: length of the period (in days)
+        df: the data only with columns (`'DATE'`, `'regime'`, ...) (the other columns are returns).
+        freq_alias (str | None): alias for the length of the return period for average return and std. This can be `'W'`, `'2W'`, `'ME'`. 
+            Default is `None` for 1 day. Full list see https://pandas.pydata.org/pandas-docs/stable/user_guide/timeseries.html#offset-aliases.
+        freq_length (int): length of the return period (in days). Default to `1`.
 
-    returns:
-        results: dict with regimes as keys as dataframe as values. The dataframe has indices as indices and metrics as columns
-
-    '''
+    Returns:
+        results (dict): dict with regimes as keys as dataframe as values. The dataframe has indices as indexes and metrics as columns.
+    """
 
     results = df.groupby('regime').apply(
         calculate_return_metrics,
@@ -157,19 +146,18 @@ def calculate_return_metrics_last_date_of_period(
         freq_alias: str | None = None, 
         freq_length: int | None = 1
 ) -> pd.DataFrame:
-    '''Calculate average return, std, annualized return, annualized std, sharpe ratio for the last day of a period within each regime
+    """
+    Calculate average return, std, annualized return, annualized std, sharpe ratio for the last day of a period within each regime.
     
     Args:
-        df: the data with and only with columns ('DATE', 'regime', ...) (the other columns are returns)
-        regimes_df: DateFrame of the regimes with columns('DATE', 'regime')
-        freq_alias: alias for the length of the period, can be 'W', '2W', 'ME'. default is None for 1 day
-            full list see https://pandas.pydata.org/pandas-docs/stable/user_guide/timeseries.html#offset-aliases
-        freq_length: length of the period (in days)
+        df (DataFrame): the data only with columns (`'DATE'`, `'regime'`, ...) (the other columns are returns).
+        freq_alias (str | None): alias for the length of the return period for average return and std. This can be `'W'`, `'2W'`, `'ME'`. 
+            Default is `None` for 1 day. Full list see https://pandas.pydata.org/pandas-docs/stable/user_guide/timeseries.html#offset-aliases.
+        freq_length (int): length of the return period (in days). Default to `1`.
 
-    returns:
-        results: dict with regimes as keys and dataframe as values. The dataframe has indices as indices and metrics as columns
-
-    '''
+    Returns:
+        results (dict): dict with regimes as keys and dataframe as values. The dataframe has indices as indexes and metrics as columns
+    """
     
     # get the last day of each period
     df = df[df['regime'] != df['regime'].shift(-1)]
@@ -188,16 +176,16 @@ def calculate_returns_forward(
         df: pd.DataFrame, 
         period: int
 ):
-    '''Calculate returns in the following period days after each regime
+    """
+    Calculate returns in the following `period` days for each regime.
     
     Args:
-        df: the data with columns (DATE, .......) (the other columns are returns)
-        regimes: the regime labels for each time stamp
-        period: length of the period
+        df (DataFrame): the data with columns (`'DATE'`, `'regime'`, ...) (the other columns are returns).
+        period (int): length of the period.
 
     Returns:
-        results: dataframe with indices as indices, regime as columns and returns as rows
-    '''
+        results: dataframe with indices as indexes, regime as columns and returns as rows.
+    """
 
     returns_cum = df.sort_values(by='DATE', ascending=False).drop(columns=['DATE', 'regime']).rolling(period).sum().shift(1)
     returns_cum = pd.merge(df[['DATE','regime']], returns_cum, left_index=True, right_index=True).dropna().sort_values(by='DATE', ascending=True)
@@ -213,50 +201,33 @@ def calculate_returns_forward(
 
 
 def smooth_regimes(regimes: np.ndarray, window: int | None = 10):
-    '''Smooth the regimes, by assign the most frequent regime within the sliding window
+    """
+    Smooth the regimes, by assigning the most frequent regime within the sliding window.
     
     Args:
-        regimes: the regime identification results
-        window: length of the sliding window
+        regimes (ndarray): the regime identification results.
+        window (int): length of the sliding window.
 
     Returns:
-        
-        
-    '''
+        regimes_series (Series): Series of the smoothed regimes.
+    """
+
     regimes_series = pd.Series(regimes)
-    regimes_series = regimes_series.rolling(window,min_periods=1).apply(lambda x : x.mode()[0])
+    regimes_series = regimes_series.rolling(window, min_periods=1).apply(lambda x : x.mode()[0])
     regimes_series = regimes_series.round()
 
     return regimes_series
 
 
-def calculate_transition_matrix_regime_level(regimes_df: pd.DataFrame) -> pd.DataFrame:
-    '''Calculate transition matrix at regime level
-    
-    Args:
-        regimes_df: DateFrame of the regimes which must include columns('DATE', 'regime')
-    '''
-
-    return calculate_transition_matrix(regimes_df, at_regimes_level=True)
-
-
-def calcualte_trainsition_matrix_date_level(regimes_df: pd.DataFrame) -> pd.DataFrame:
-    '''Calculate transition matrix at date level
-    
-    Args:
-        regimes_df: DateFrame of the regimes which must include columns('DATE', 'regime')
-    '''
-
-    return calculate_transition_matrix(regimes_df, at_regimes_level=False)
-
-
 def calculate_transition_matrix(regimes_df: pd.DataFrame, at_regimes_level: bool | None = True) -> pd.DataFrame:
-    '''Calculate transition matrix
+    """
+    Calculate transition matrix.
     
     Args:
-        regimes_df: DateFrame of the regimes which must include columns('DATE', 'regime')
-        at_regimes_level: if calculate regime at regime level. If true, the probability between the same regime would be zero
-    '''
+        regimes_df (DataFrame): DateFrame of the regimes which must include columns(`'DATE'`, `'regime'`)
+        at_regimes_level (bool): If to calculate regime at regime level. If `'True'`, the probability between the same regime would be zero.
+            If `'False'`, the transition matrix is at the data level.
+    """
     # ensure the regimes are in ascending order of the dates
     regimes_df = regimes_df.sort_values(by='DATE', ascending=True)
     regimes_df = regimes_df[['DATE', 'regime']]
@@ -275,68 +246,53 @@ def calculate_transition_matrix(regimes_df: pd.DataFrame, at_regimes_level: bool
 
 
 def calculate_rwo_entropy(row: Union[np.ndarray, pd.Series]) -> float:
-    '''Calculate entropy for the rwo
+    """
+    Calculate entropy for the row of probabilities.
     
     Args:
-        row: row of probabilities
+        row (ndarray | Series): row of probabilities.
 
     Returns:
-        entropy
-    '''
+        entropy (float): entropy of the row.
+    """
     row = row[row>0]
     return -np.sum(row * np.log(row))
 
 
 def calculate_transition_matrix_entropy(matrix: pd.DataFrame, apply_norm: bool | None = False) -> float:
-    '''Calculate the entropy for this trainsition matrix:
+    """
+    Calculate the entropy for this trainsition matrix.
     
     Args:
-        matrix: the transition matrix
-        apply_norm: If Ture, average the entropies of rows
+        matrix (DataFrame): the transition matrix, with regimes as indexes and regimes as columns.
+            Value at row i column j means the probability of regime i switching to regime j.
+        apply_norm: If `Ture`, normalize the entropy by number_of_rows x log(number_of_rows).
 
     Returns:
-        entropy
-    '''
+        entropy (float): entropy of the matrix.
+    """
+
     entropies_rows = np.apply_along_axis(calculate_rwo_entropy, 1, matrix)
     if apply_norm:
-        return np.sum(entropies_rows) / np.log(len(entropies_rows)) / len(entropies_rows)
+        return np.sum(entropies_rows) / np.log(len(entropies_rows)-1) / len(entropies_rows)
     else:
         return np.sum(entropies_rows)
     
 
 def analyse_regimes_durations(regimes_df: pd.DataFrame) -> pd.DataFrame:
-    '''Analyse statistics of regime durations
+    """
+    Calculate mean, std, 25, 50, 70 quantiles for regime durations and the total days and 
+        the corresponding percentage for each regime and all regime periods.
     
     Args: 
-        regimes_df: dataframe must include columns('DATE', 'regime')
+        regimes_df (DataFrame): DataFrame with columns (`'DATE'`, `'regime'`). Other columns will be ignored.
 
     Returns:
-        regimes_durations_stats: dataframe with columns ('regime', 'mean','std','count','25 quantile', 'median', '75 quantile', 'percent')
-    '''
+        regimes_durations_stats (DataFrame): DataFrame with columns 
+            (`'regime'`, `'mean'`, `'std'`, `'count'`, `'25 quantile'`, `'median'`, `'75 quantile'`, `'percent'`).
+    """
     regimes_df = regimes_df[['DATE','regime']]
     regimes_durations = calculate_regime_durations(regimes_df)
     regimes_durations_stats = calculate_regime_durations_statistics(regimes_durations)
 
     return regimes_durations_stats
-
-
-if __name__ == '__main__':
-
-    regimes = {
-        'DATE': pd.date_range(start='2023-01-01', periods=20, freq='D'),
-        'regime': np.random.choice([0,1,2,3], 20, replace=True),
-        'price1': np.random.random(20),
-        'price2': np.random.random(20)
-    }
-
-    regimes_df = pd.DataFrame(regimes)
-
-    print(regimes_df)
-    calculate_returns_forward(regimes_df, 10)
-
-
-# print(regimes_df)
-# print(calculate_transition_matrix_regime_level(regimes_df))
-# print(calcualte_trainsition_matrix_date_level(regimes_df))
-# print(calculate_regime_durations(regimes_df))
-# print(calculate_regime_durations_statistics((calculate_regime_durations(regimes_df))))